1b5ba2117f7d063a01e60ddeb19cf89d46af30cb
mardoqueu <mdq767@gmail.com>
2022-09-27 18:13:12 -0300

    todo-list compolete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import {Todo} from './Todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-list';
+  todos: Todo[] = [];
+  newTodo: string;
+
+
+saveTodo(){
+  if(this.newTodo){
+    let todo = new Todo();
+    todo.name = this.newTodo;
+    todo.isCompleted = true;
+    this.todos.push(todo);
+    this.newTodo = "";
+  }else{
+    alert('Please Enter Todo');
+  }
+}
+
+done(id:number){
+  this.todos[id].isCompleted = !this.todos[id].isCompleted;
+}
+
+remove(id:number){
+  this.todos = this.todos.filter((v,i)=> i !== id);
+}
+
 }
